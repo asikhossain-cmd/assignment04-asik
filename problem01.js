33310b698a56694c31987850d1d0103971a14a76
Asik <asikhossain592445@gmail.com>
2025-08-14 14:54:41 +0600

    problem - 02 solved

--- problem01.js
+++ problem01.js
@@ -6,6 +6,20 @@ function totalFine( fare ) {
     return "Invalid";
 }
 
-// const result = totalFine(50);
+// const result = totalFine(200);
 // console.log(result);
+
+
+function  onlyCharacter( str ) {
+    if(typeof str == "string") {
+        let stringArray = str.split(" ");
+        let problemSolved = stringArray.join("").toUpperCase();
+        // problemSolve.toUpperCase();
+        return problemSolved;
+    }
+    return"Invalid";
+}
+
+let sampleString = onlyCharacter("he ll o");
+console.log(sampleString);
 
---
+++ problem02.js
@@ -0,0 +1,12 @@
+function  onlyCharacter( str ) {
+    if(typeof str == "string") {
+        let stringArray = str.split(" ");
+        let problemSolved = stringArray.join("").toUpperCase();
+        // problemSolve.toUpperCase();
+        return problemSolved;
+    }
+    return"Invalid";
+}
+
+// let sampleString = onlyCharacter("  h e llo wor   ld");
+// console.log(sampleString);
