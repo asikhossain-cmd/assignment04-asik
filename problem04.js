cd2581437ed53b6ae2adff72dea472a23fab4127
Asik <asikhossain592445@gmail.com>
2025-08-15 16:10:27 +0600

    solved problem04.js

--- problem04.js
+++ problem04.js
@@ -1,3 +1,17 @@
 function  isSame(arr1 , arr2 ) {
-    
+    if(Array.isArray(arr1) && Array.isArray(arr2)) {
+        for(let i = 0; i < arr1.length; i++) {
+            if(arr1[i] !== arr2[i]) {
+                return false;
+            } 
+        }
+        return (arr1.length == arr2.length);
+    }
+    return"Invalid";
 }
+
+
+const array1 = [5,4,6,10];
+const array2 = [5,4,6];
+const output = isSame(array1, array2);
+console.log(output);
